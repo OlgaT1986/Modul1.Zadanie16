0f1e76c11335106fb528d3801088c2b34d4da3f3
Ольга <evinn86@mail.ru>
2022-01-27 16:55:16 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie16.2/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie16._2
+{
+    class Product
+    {
+        public int codeProduct { get; set; }
+        public string nameProduct { get; set; }
+        public double priceProduct { get; set; }
+    }
+}
+
---
+++ Modul1.Zadanie16.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Modul1.Zadanie16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Empty - пустые кавычки
+            string jsonString = String.Empty;  
+            using (StreamReader sr = new StreamReader("../../../Product.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            // перевод строки jsonString в массив <Product[]>
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+            Product maxProduct = products[0];
+            foreach (Product p in products)
+            {
+                if (p.priceProduct > maxProduct.priceProduct)
+                {
+                    maxProduct = p;
+                }
+            }
+            Console.WriteLine($"{maxProduct.codeProduct} {maxProduct.nameProduct} {maxProduct.priceProduct}");
+            Console.ReadKey();
+        }
+    }
+}
